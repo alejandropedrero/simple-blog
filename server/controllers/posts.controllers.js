699eed6e91f7c264c10020d6957059d79e631dd9
alejandropedrero <apedrerovega@gmail.com>
2023-07-01 19:03:19 +0200

    manejo errores backend

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -1,47 +1,69 @@
 import { pool } from "../db.js";
 
 export const getPosts = async (req, res) => {
-  const [result] = await pool.query("SELECT * FROM posts ORDER BY created ASC");
-  res.json(result);
+  try {
+    const [result] = await pool.query(
+      "SELECT * FROM posts ORDER BY created DESC"
+    );
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const getPost = async (req, res) => {
-  const [result] = await pool.query("SELECT * FROM posts WHERE id = ?", [
-    req.params.id,
-  ]);
+  try {
+    const [result] = await pool.query("SELECT * FROM posts WHERE id = ?", [
+      req.params.id,
+    ]);
 
-  if (result.length === 0) {
-    return res.status(404).json({ message: "Post no encontrado" });
+    if (result.length === 0) {
+      return res.status(404).json({ message: "Post no encontrado" });
+    }
+
+    res.json(result[0]);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
-
-  res.json(result[0]);
 };
 
 export const createPost = async (req, res) => {
-  const { title, content } = req.body;
-  const [result] = await pool.query(
-    "INSERT INTO posts(title, content) VALUES (?, ?)",
-    [title, content]
-  );
-  res.json({ id: result.insertId, title, content });
+  try {
+    const { title, content } = req.body;
+    const [result] = await pool.query(
+      "INSERT INTO posts(title, content) VALUES (?, ?)",
+      [title, content]
+    );
+    res.json({ id: result.insertId, title, content });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const updatePost = async (req, res) => {
-  const result = await pool.query("UPDATE posts SET ? WHERE id = ?", [
-    req.body,
-    req.params.id,
-  ]);
-  res.json(result);
+  try {
+    const result = await pool.query("UPDATE posts SET ? WHERE id = ?", [
+      req.body,
+      req.params.id,
+    ]);
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const deletePost = async (req, res) => {
-  const [result] = await pool.query("DELETE FROM posts WHERE id = ?", [
-    req.params.id,
-  ]);
+  try {
+    const [result] = await pool.query("DELETE FROM posts WHERE id = ?", [
+      req.params.id,
+    ]);
 
-  if (result.affectedRows === 0) {
-    return res.status(404).json({ message: "Post no encontrado" });
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "Post no encontrado" });
+    }
+
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
-
-  return res.sendStatus(204);
 };
