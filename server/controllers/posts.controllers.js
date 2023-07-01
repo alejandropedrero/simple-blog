b136105da099b82ed622e0d40e31c03f70cbe80d
alejandropedrero <apedrerovega@gmail.com>
2023-07-01 18:52:56 +0200

    acabados los post controllers

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -1,11 +1,20 @@
 import { pool } from "../db.js";
 
-export const getPosts = (req, res) => {
-  res.send("Obteniendo todos los posts");
+export const getPosts = async (req, res) => {
+  const [result] = await pool.query("SELECT * FROM posts ORDER BY created ASC");
+  res.json(result);
 };
 
-export const getPost = (req, res) => {
-  res.send("Obteniendo un post");
+export const getPost = async (req, res) => {
+  const [result] = await pool.query("SELECT * FROM posts WHERE id = ?", [
+    req.params.id,
+  ]);
+
+  if (result.length === 0) {
+    return res.status(404).json({ message: "Post no encontrado" });
+  }
+
+  res.json(result[0]);
 };
 
 export const createPost = async (req, res) => {
@@ -17,10 +26,22 @@ export const createPost = async (req, res) => {
   res.json({ id: result.insertId, title, content });
 };
 
-export const updatePost = (req, res) => {
-  res.send("Actualizando post");
+export const updatePost = async (req, res) => {
+  const result = await pool.query("UPDATE posts SET ? WHERE id = ?", [
+    req.body,
+    req.params.id,
+  ]);
+  res.json(result);
 };
 
-export const deletePost = (req, res) => {
-  res.send("Eliminando post");
+export const deletePost = async (req, res) => {
+  const [result] = await pool.query("DELETE FROM posts WHERE id = ?", [
+    req.params.id,
+  ]);
+
+  if (result.affectedRows === 0) {
+    return res.status(404).json({ message: "Post no encontrado" });
+  }
+
+  return res.sendStatus(204);
 };
